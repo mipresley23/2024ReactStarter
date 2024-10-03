a162910b3d8e0871d8e5527d88023bd1cf21a1bc
Michael Presley <mpresley@hollandspecial.com>
2024-10-03 15:23:49 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/home";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const [loaded, setLoaded] = useState(false);
+
+    useEffect(() => {
+        setLoaded(true);
+    }, [])
+
+    if(!loaded){
+        return null;
+    }
+
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" exact={true} element={<Home />} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
-
-export default App;
---
+++ src/components/home/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Home() {
+    return (
+        <>
+            <h1>
+                We made it to the home page!
+            </h1>
+        </>
+    )
+}
